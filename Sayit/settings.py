8e1f04262cea5dbb2cf24d9c5a020006eea1c36e
Mthabs <>
2023-09-29 01:49:50 +0000

    add post model

--- Sayit/settings.py
+++ Sayit/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['sayit2023.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['8000-mthabs-say-it-kn84mfm9qz.us2.codeanyapp.com']
 
 
 # Application definition
