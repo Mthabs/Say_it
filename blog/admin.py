10a3e376c87bd76a49aaeddaf9d38a847d6c7885
Mthabs <>
2023-09-29 02:56:30 +0000

    add admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
 # Register your models here.
+
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
